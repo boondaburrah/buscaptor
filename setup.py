0fabbd448f541b8aaff6942e3682cb6021d51163
Luke Berry <boondaburrah@airedale.local>
2015-05-03 21:38:15 -0400

    changing setup info

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
+setup(name='Buscaptor',
+      version='0.1',
+      description='Bridge between alexa and various transit data sources',
+      author='boondaburrah',
       author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+      url='http://github.com/boondaburrah/buscaptor',
 #      install_requires=['Django>=1.3'],
      )
